0db8eef6bbc229eea1c075ff3cdb85c74930efa9
Zoneam <zoneam@gmail.com>
2022-04-01 15:02:41 -0700

    working version

--- script/script.js
+++ script/script.js
@@ -1,58 +1,87 @@
 let winningSlots = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    ['0', '1', '2'],
+    ['3', '4', '5'],
+    ['6', '7', '8'],
+    ['0', '3', '6'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['0', '4', '8'],
+    ['2', '4', '6']
 ];
 
-let turn = 'X';
-let inputArray = new Array(9);
-let xArr = [];
-let oArr = [];
-let winner = '';
+let gameBoard = `<tr>
+                <td id="slot-0"></td>
+                <td id="slot-1"></td>
+                <td id="slot-2"></td>
+                </tr>
+                <tr>
+                <td id="slot-3"></td>
+                <td id="slot-4"></td>
+                <td id="slot-5"></td>
+                </tr>
+                <tr>
+                <td id="slot-6"></td>
+                <td id="slot-7"></td>
+                <td id="slot-8"></td>
+                </tr>`
 
+let gameTable = document.querySelector('table');               
 let title = document.querySelector('h1');
+
 document.querySelector('#start').addEventListener('click', GameStart);
 
-let tds = document.querySelectorAll('td');
-
-tds.forEach((td)=>{
-    td.addEventListener('click', (e)=> {
-         let inputSlot = e.target.id[e.target.id.length-1];
-         if(turn === 'X') {
-            inputArray[inputSlot] = 'X';
-            e.target.innerHTML = '<div class="user-input-X">X</div>';
-            turn = "O";
-         } else {
-            inputArray[inputSlot] = 'O';
-            e.target.innerHTML = '<div class="user-input-O">O</div>';
-            turn = "X";
-         }
-         // putting xs and os into arrays
-         inputArray.forEach((input,i)=>{
-            if (input === 'X') xArr[i] = i;
-            if (input === 'O') oArr[i] = i;
-         })
-         // checking if there is winner or not
-          winningSlots.forEach((winningSlot)=>{
-            if(winningSlot.every((el) => {
-                 return xArr.includes(el);
-            })) return winner = "X"; 
-            if(winningSlot.every((el) => {
-                return oArr.includes(el);
-           })) return winner = "O"; 
-         })
-         // Winner
-         winner?title.textContent = (winner + " Wins !!!"):title.textContent = ("There's no winner yet");
-
-    }, {once : true});
-})
-
 function GameStart() {
+    let turn = 'X';
+    let xArr = [];
+    let oArr = [];
+    let winner = '';
+    gameTable.innerHTML = gameBoard;
+    let tds = document.querySelectorAll('td');
+    title.textContent = "There's no winner yet";
 
+    tds.forEach((td)=>{
+        td.addEventListener('click', (e)=> {
+
+             let inputSlot = e.target.id[e.target.id.length-1];
+
+             if(turn === 'X') {
+
+                xArr.push(inputSlot);
+                e.target.innerHTML = '<div class="user-input-X">X</div>';
+                turn = "O";
+             } else {
+
+                oArr.push(inputSlot);
+                e.target.innerHTML = '<div class="user-input-O">O</div>';
+                turn = "X";
+             }
+
+              winningSlots.forEach((winningSlot) => {
+
+                if( winningSlot.every((el) => {
+                    return xArr.includes(el);
+                })) winner = 'X';
+
+                if( winningSlot.every((el) => {
+                    return oArr.includes(el);
+                })) winner = 'O';
+
+             });
+             console.log(xArr.length + oArr.length)
+             if (xArr.length + oArr.length === 9) {
+                 title.textContent = ("It's a Tie");
+             } else {
+                 // Winner
+                if(winner){
+                    tds.forEach (td => {
+                        td.classList.add('disabled');
+                    })
+                    title.textContent = (winner + " Wins !!!")
+                } else title.textContent = ("There's no winner yet");
+             }
+    
+        }, {once : true});
+    })
 };
 
+GameStart();
