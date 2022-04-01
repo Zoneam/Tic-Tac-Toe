58851ef2d820016d0e862e3b8ebb154a2663adee
Zoneam <zoneam@gmail.com>
2022-04-01 16:36:05 -0700

    refactored code

--- script/script.js
+++ script/script.js
@@ -27,61 +27,69 @@ let gameBoard = `<tr>
 
 let gameTable = document.querySelector('table');               
 let title = document.querySelector('h1');
-
+let turn = 'X';
+let xArr = [];
+let oArr = [];
+let winner = '';
 document.querySelector('#start').addEventListener('click', GameStart);
 
+//Start and Restart the Game
 function GameStart() {
-    let turn = 'X';
-    let xArr = [];
-    let oArr = [];
-    let winner = '';
+    turn = 'X';
+    xArr = [];
+    oArr = [];
+    winner = '';
     gameTable.innerHTML = gameBoard;
-    let tds = document.querySelectorAll('td');
     title.textContent = "There's no winner yet";
-
-    tds.forEach((td)=>{
-        td.addEventListener('click', (e)=> {
-
-             let inputSlot = e.target.id[e.target.id.length-1];
-
-             if(turn === 'X') {
-
-                xArr.push(inputSlot);
-                e.target.innerHTML = '<div class="user-input-X">X</div>';
-                turn = "O";
-             } else {
-
-                oArr.push(inputSlot);
-                e.target.innerHTML = '<div class="user-input-O">O</div>';
-                turn = "X";
-             }
-
-              winningSlots.forEach((winningSlot) => {
-
-                if( winningSlot.every((el) => {
-                    return xArr.includes(el);
-                })) winner = 'X';
-
-                if( winningSlot.every((el) => {
-                    return oArr.includes(el);
-                })) winner = 'O';
-
-             });
-             console.log(xArr.length + oArr.length)
-             if (xArr.length + oArr.length === 9) {
-                 title.textContent = ("It's a Tie");
-             } else {
-                 // Winner
-                if(winner){
-                    tds.forEach (td => {
-                        td.classList.add('disabled');
-                    })
-                    title.textContent = (winner + " Wins !!!")
-                } else title.textContent = ("There's no winner yet");
-             }
-    
+    let tds = document.querySelectorAll('td');
+    tds.forEach( td => {
+        td.addEventListener('click', e => {
+            clickTracker(e);
+            checkForWins();
+            displayWinner(tds);
         }, {once : true});
     })
 };
 
+// Taking Turns
+function clickTracker(e) {
+    let inputSlot = e.target.id[e.target.id.length-1];
+             if(turn === 'X') {
+                xArr.push(inputSlot);
+                e.target.innerHTML = '<div class="user-input-X">X</div>';
+                turn = "O";
+             } else {
+                oArr.push(inputSlot);
+                e.target.innerHTML = '<div class="user-input-O">O</div>';
+                turn = "X";
+             };
+};
+
+// Checking for winners
+function checkForWins(){
+    winningSlots.forEach((winningSlot) => {
+        if( winningSlot.every(el => {
+            return xArr.includes(el);
+        })) winner = 'X';
+        if( winningSlot.every(el => {
+            return oArr.includes(el);
+        })) winner = 'O';
+     });
+};
+
+// Displaying Winner
+function displayWinner(tds){
+    if (xArr.length + oArr.length === 9) {
+        title.textContent = ("It's a Tie");
+    } else {
+       if(winner){
+           tds.forEach (td => {
+               td.classList.add('disabled');
+           })
+           title.textContent = (`${winner} won in ${winner === 'X'?xArr.length:oArr.length} steps`)
+       } else title.textContent = ("There's no winner yet");
+    }
+};
+
+// initialization
 GameStart();
